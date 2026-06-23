cadef1576c325d7661e0252e546678f1006eeb8a
Zaynab Othman <zay.taha@gmail.com>
2026-06-23 19:14:14 +0300

    add fetch handler to service worker

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,7 @@
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
+})
+
 self.addEventListener('install', e => self.skipWaiting())
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()))
 
